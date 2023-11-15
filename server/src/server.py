a1e713ed9c45bff96d31f9b62b71820fc9f2f794
pnavab <114110926+pnavab@users.noreply.github.com>
2023-11-14 22:46:23 -0800

    create connection manager

--- server/src/server.py
+++ server/src/server.py
@@ -1,22 +1,55 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import uvicorn
+import uuid
+import json
+from dataclasses import dataclass
+
+@dataclass
+class ConnectionManager:
+    def __init__(self) -> None:
+        self.active_connections: dict = {}
+    
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        id = str(uuid.uuid4())
+        self.active_connections[id] = websocket
+        
+        await self.send_message_to(websocket, json.dumps({ "type": "connect", "id": id }))
+    
+    def disconnect(self, websocket: WebSocket):
+        id = self.find_connection_id(websocket)
+        del self.active_connections[id]
+
+    def find_connection_id(self, websocket: WebSocket):
+        val_list = list(self.active_connections.values())
+        key_list = list(self.active_connections.keys())
+        id = val_list.index(websocket)
+        return key_list[id]
+    
+    async def send_message_to(self, websocket: WebSocket, message: str):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections.values():
+            await connection.send_text(message)
 
 app = FastAPI()
 
-@app.get("/")
-async def blank():
-    return {"message": "hello world"}
+connection_manager = ConnectionManager()
+
 
 @app.websocket("/messaging")
 async def websocket_endpoint(websocket: WebSocket):
-  await websocket.accept()
-  try:
-    while True:
-        data = await websocket.receive_text()
-        print(f"Received data {data}")
-        await websocket.send_text(data)
-  except WebSocketDisconnect:
-         print(f"client disconnected")
+    await connection_manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Received data {data}")
+            await connection_manager.broadcast(data)
+    except WebSocketDisconnect:
+        print(f"client disconnected")
+        id = connection_manager.disconnect(websocket)
+        await connection_manager.broadcast(json.dumps({ "type": "disconnected", "id": id }))
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
